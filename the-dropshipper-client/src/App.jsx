441f32b81aecae7bc2d2f38e2d9483b154248ff9
Marko <mk40k@quantentunnel.de>
2026-05-22 14:00:05 +0200

    Commit 5

--- the-dropshipper-client/src/App.jsx
+++ the-dropshipper-client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import mk40k from './assets/mk40k.png'
 import './App.css'
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={mk40k} className="framework" alt="MK 40K logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
